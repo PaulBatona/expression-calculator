d8fa2a99ff029bdd2a5268aa764cc486ad75a06d
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-03-22 14:38:27 +0300

    feat: some script

--- src/index.js
+++ src/index.js
@@ -5,6 +5,30 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let openBrackets = expr.split('').filter(e=>e==='(').length;
+    let closeBrackets = expr.split('').filter(e=>e===')').length;
+    let noSpace = expr.split('').filter(e=>e!==' ').join('');
+    if(openBrackets!==closeBrackets){
+        throw new Error('ExpressionError: Brackets must be paired')
+    }
+    if(noSpace.includes('/0')){
+        throw new Error('TypeError: Division by zero.')
+    }
+
+    function calc(i1, operator, i2){
+        if(operator=='+'){
+            return i1+i2;
+        }
+        else if(operator=='-'){
+            return i1-i2;
+        }
+        else if(operator=='*'){
+            return i1*i2;
+        }
+        else if(operator=='/'){
+            return i1/i2
+        }
+    }
 }
 
 module.exports = {
